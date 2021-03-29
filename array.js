0614ce80f480ca7a7f25e445be1df4c1cd5c07ad
Wander Souza <wanderrodrigues_@outlook.com>
2021-03-29 12:45:46 -0300

    javascript

---
+++ array.js
@@ -0,0 +1,16 @@
+/*
+
+array (vetores)
+    *uma lista
+    *agrupamento de dado
+
+
+    ["sting", 23, float]
+*/
+
+console.log([
+    "leite",
+    "ovos",
+    2,
+    3
+])
---
+++ boolean.js
@@ -0,0 +1,20 @@
+/*
+Boolean
+
+    somente 2 valores
+
+    true // verdadeiro
+    false // falso
+
+*/
+console.log('false')
+
+/*
+undefined
+    *indefinido
+
+null
+    *nulo
+    *objeto que não possui nada dentro
+    *diferente de indefinido
+*/
