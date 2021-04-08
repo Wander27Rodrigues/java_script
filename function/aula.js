43665c1c56ed0310555e09a59dec2a14b1866f18
Wander Souza <wanderrodrigues_@outlook.com>
2021-04-07 23:17:25 -0300

    number

--- function/aula.js
+++ function/aula.js
@@ -11,6 +11,9 @@ const sum = function (number1, number2) {
 
 sum(2, 3) // arguments - argumentos da fumction que foi executada
 
+let number1 = 34
+let number2 = 25
+
 console.log(`o numero 1 é ${number1}`)
 console.log(`o numero 2 é ${number2}`)
 console.log(`a soma é ${number1, number2}`)
