be68696f202eb3ea4ce343f10be35f4b9c1484bd
Wander Souza <wanderrodrigues_@outlook.com>
2021-04-07 23:23:21 -0300

    num

---
+++ function/arguments.js
@@ -0,0 +1,10 @@
+// function intro variavel
+// function anonymous
+
+
+// parâmetros (parameters)
+const sum = function (number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // arguments - argumentos da fumction que foi executada
