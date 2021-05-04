85531fffc850c99f1f79fbf3471c9e579fa1cc47
Wander Souza <wanderrodrigues_@outlook.com>
2021-05-04 10:36:21 -0300

    :brain: controFlow

---
+++ expressions/operatorsLogical.js
@@ -0,0 +1,22 @@
+// Operadores Lógicos (logical operators)
+
+// -2 valores booleanos, quando verificados,
+// resultará em true and false
+
+//AND - &&
+let pao = true
+let queijo = true
+
+console.log(pao && queijo)
+
+// OR - ||
+let pao = true
+let queijo = false
+
+console.log(pao || queijo)
+
+// NOT - !
+let pao = true
+let queijo = false
+
+console.log(!pao)
